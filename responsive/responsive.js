ac0e9f17615856bcfd6d8aab5174d639b067bfb9
ananthsekar007 <ananthsekar007@gmail.com>
2023-05-12 11:24:51 +0530

    [Ananth] Adds responsive html

---
+++ responsive/responsive.js
@@ -0,0 +1,17 @@
+const images = [
+    "https://media.gettyimages.com/id/1288394573/photo/australia-v-india-odi-game-2.jpg?s=612x612&w=gi&k=20&c=KOd0_A6Y2oXjhFmMDHi4Wf0wdymuq5hIAdG9BGQ-jiA=",
+    "https://media.gettyimages.com/id/1288966957/photo/australia-v-india-odi-game-3.jpg?s=612x612&w=gi&k=20&c=2L1eZ8MYT4jcyxRUpNOPrJmAtqY8QjX6W3vWYbuhiKU=",
+    "https://media.gettyimages.com/id/1309238272/photo/india-v-england-2nd-one-day-international.jpg?s=612x612&w=gi&k=20&c=xCSfNhNIlMKnZjHYHDAo4l99BzUsNpDjXEeGe11D1gA=",
+    "https://media.gettyimages.com/id/1440476830/photo/india-v-england-icc-mens-t20-world-cup-semi-final.jpg?s=612x612&w=gi&k=20&c=Q6KGRt8KPmmH0aLeAtgtUeqTcpKeD2EGbfktHmn_8MY=",
+    "https://media.gettyimages.com/id/1434940895/photo/india-icc-mens-t20-cricket-world-cup-2022-team-headshots.jpg?s=612x612&w=gi&k=20&c=xqekF77F683O5Cus-MzwG-2J5g-tM2RAN-2Qj0J7Y_k=",
+    "https://media.gettyimages.com/id/1132521368/photo/india-v-australia-t20i-game-2.jpg?s=612x612&w=gi&k=20&c=SguvvX9WuNg_JYSzDbL3kCwebVdNVxTcbESmCbVJGLY=",
+    "https://media.gettyimages.com/id/1436938531/photo/india-v-netherlands-icc-mens-t20-world-cup.jpg?s=612x612&w=gi&k=20&c=rFUjinw6kvAvA8WA5A9p6EmTixSoXE9BhczhZjNZRhE=",
+    "https://media.gettyimages.com/id/1472909629/photo/india-v-australia-4th-test-day-4.jpg?s=612x612&w=gi&k=20&c=K3iYfy7ZtxF6OdbXmOeZg_KOgmnZ_rfZ_hC6pDF2m6g=",
+    "https://media.gettyimages.com/id/1020059034/photo/specsavers-3rd-test-day-three.jpg?s=612x612&w=gi&k=20&c=Mt5NNMNh0lJIcEmxbopxC-O44fVuU0MxoWn3FB9IZFA=",
+    "https://media.gettyimages.com/id/1435854325/photo/india-v-pakistan-icc-mens-t20-world-cup.jpg?s=612x612&w=gi&k=20&c=KArxnTxFkiKTQhOW91j25MPUD1513s5NJB7c3JtlYyM=",
+    "https://media.gettyimages.com/id/1083068838/photo/australia-v-india-odi-game-2.jpg?s=612x612&w=gi&k=20&c=sKKqdQPqWGAmgNWBrxzzWtGrmTS1C3DxA_XQzezn2F4=",
+    "https://media.gettyimages.com/id/1338718098/photo/england-v-india-fourth-lv-insurance-test-match-day-five.jpg?s=612x612&w=gi&k=20&c=yruPkk81rYoeOQvK-FCouB4wqMpbmjZJEkFbBep2ahY=",
+    "https://media.gettyimages.com/id/1409305501/photo/england-v-india-3rd-royal-london-series-one-day-international.jpg?s=612x612&w=gi&k=20&c=2JHdU5tzLSsAQmMlJBEwOV3vf2R914fiwKpsRAjJ52c=",
+    "https://media.gettyimages.com/id/1012037708/photo/specsavers-1st-test-day-two.jpg?s=612x612&w=gi&k=20&c=AbfFxjePQXmj4nV3zh4mscbihKGkHDMy5HJnrkKUWoM=",
+    "https://media.gettyimages.com/id/1308098673/photo/india-v-england-5th-t20-international.jpg?s=612x612&w=gi&k=20&c=BvvtNekGs2Qc450578lCaHcznY5Xz-cTiDfoIoP13GA="
+]
